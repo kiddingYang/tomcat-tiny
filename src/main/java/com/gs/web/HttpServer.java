0af54dc4103513999f0505297f1ad158ca26f630
jyang <jyang@grandstream.cn>
2022-01-21 14:07:38 +0000

    ch01

---
+++ src/main/java/com/gs/web/HttpServer.java
@@ -0,0 +1,61 @@
+package com.gs.web;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpServer {
+
+
+    public static final String WEB_ROOT = System.getProperty("user.dir") + File.separator + "webroot";
+
+    public static final String SHUTDOWN_COMMAND = "/STUTDOWN";
+
+    public boolean shutdown = false;
+
+    public static void main(String[] args) {
+        HttpServer server = new HttpServer();
+        server.await();
+    }
+
+    private void await() {
+        ServerSocket serverSocket = null;
+        int port = 8989;
+        try {
+            serverSocket = new ServerSocket(port, 1, InetAddress.getByName("127.0.0.1"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        while (!shutdown) {
+            Socket socket = null;
+            InputStream input = null;
+            OutputStream output = null;
+            try {
+                socket = serverSocket.accept();
+                input = socket.getInputStream();
+                output = socket.getOutputStream();
+
+                Request request = new Request(input);
+                request.parse();
+
+                Response response = new Response(output);
+                response.setRequest(request);
+
+                response.sendStaticResource();
+
+                socket.close();
+
+                shutdown = request.getUri().equals(SHUTDOWN_COMMAND);
+            } catch (Exception e) {
+                e.printStackTrace();
+                continue;
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/gs/web/PrimitiveServlet.java
@@ -0,0 +1,4 @@
+package com.gs.web;
+
+public class PrimitiveServlet {
+}
